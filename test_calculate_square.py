a22313057e77f3f63da8c33b840b0803c134a2dc
MaximeLanca <lanca.maxime.ber@gmail.com>
2026-08-24 14:24:29 +0200

    First commit & First lab

---
+++ test_calculate_square.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from langchain.tools import tool
+load_dotenv()
+
+@tool
+def square_root(x:float) -> float:
+    """Calculate the square root of a number"""
+    return x ** 0.5
+
+@tool("square_root")
+def tool1 (x:float) -> float:
+    """Calculate the square root of a number"""
+    return x ** 0.5
+
+print(square_root.name)
+print(square_root.invoke({"x": 16}))
